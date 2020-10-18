25faa72c12ee4c05422cefe7f95a7775956ef061
ktc802005 <kennethcarter@me.com>
2020-10-18 11:43:06 -0400

    yahoo AI

--- src/test/java/Action_Items/Yahoo_AI.java
+++ src/test/java/Action_Items/Yahoo_AI.java
@@ -1,2 +1,24 @@
-package Action_Items;public class Yahoo_AI {
+package Action_Items;
+
+import Reusable_Library.Abstract_Class;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.asserts.SoftAssert;
+
+public class Yahoo_AI extends Abstract_Class {
+
+    @test
+
+public void Yahoo_AI throws InterruptedException {
+    logger.log(logStatus.Info,"Navigating to yahoo homepage");
+    driver.navigate().to("https://yahoo.com");
+    Thread.sleep("2000");
+
+        //verify the yahoo title using soft assertion
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(driver.getTitle(),"Yahoo");
+
+        //get count of options on left side of page
+        List<WebElement> AmountofLinksn = driver.findElements(By.xpath("href=//finance.yahoo.com/"))
+
 }
